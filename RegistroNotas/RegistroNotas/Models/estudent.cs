5077883ed40839a4da6dec53d5d3805418b30a2e
llmosquera <105768153+llmosquera@users.noreply.github.com>
2023-07-20 20:32:49 -0500

    Update estudent.cs

--- RegistroNotas/RegistroNotas/Models/estudent.cs
+++ RegistroNotas/RegistroNotas/Models/estudent.cs
@@ -6,7 +6,7 @@
 //     Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
-
+//
 namespace RegistroNotas.Models
 {
     using System;
